0594e91abfebb730a6eba83af618922bdf4fe540
Shuhei Shagawa <sgw.sgw.shu@gmail.com>
2018-06-21 11:07:15 -0700

    homepage comp

--- assets/js/commons/main.js
+++ assets/js/commons/main.js
@@ -5,23 +5,57 @@ var $ = require('jQuery');
 
 var mainNav = document.getElementById("mainNav");
 mainNav.innerHTML =
-    nav("Home", "index") +
-    subNav("What is Web design", "web_design") +
-    subNav("What is Web Development", "web_dev") +
-    subNav("What is Full Stack Developer", "full_stack_developer") +
-    subNav("What is Full Stack Web Designer", "full_stack_web_designer") +
+    nav("Home >", "index",
+        "<div class='subNav'>" +
+        "<div><a href='web_design.html'>What is Web design</div>" +
+        "<div><a href='web_dev.html'>What is Web design</a></div>" +
+        "<div><a href='web_dev.html'>What is Web Development</a></div>" +
+        "<div><a href='full_stack_developer.html'>What is Full Stack Developer</a></div>" +
+        "<div><a href='full_stack_web_designer.html'>What is Full Stack Web Designer</a></div>" +
+        "</div>") +
     nav("History", "history") +
     nav("Standards & Regulations", "standards_regulations") +
-    nav("Current Tools", "current_tools") +
-    subNav("Current Tools for Web Design", "current_tool_web_design") +
-    subNav("Current Tools for Web Development", "current_tool_web_dev") +
+    nav("Current Tools >", "current_tools",
+        "<div class='subNav'>" +
+        "<div><a href='web_design.html'>Current Tools for Web Design</a></div>" +
+        "<div><a href='web_dev.html'>Current Tools for Web Development</a></div>" +
+        "</div>") +
     nav("Articles & Resources", "article_resources") +
-    nav("Fture Technologies", "future_tech") +
-    subNav("Web Design Trend", "web_design_trends") +
-    subNav("Web Development Trend", "web_dev_trends") +
-    nav("F.A.Q", "faq") +
-    nav("The Team", "team") +
-    subNav("Ko", "about_ko") +
-    subNav("Nicole", "about_nicole") +
-    subNav("Sara", "about_sara") +
-    subNav("Shuhei", "about_shuhei");
+    nav("Fture Technologies >", "future_tech",
+        "<div class='subNav'>" +
+        "<div><a href='web_design_trends.html'>Web Design Trend</a></div>" +
+        "<div><a href='web_dev_trends.html'>Web Development Trend</a></div>" +
+        "</div>") +
+    nav("F.A.Q >", "faq") +
+    nav("The Team", "team",
+        "<div class='subNav'>" +
+        "<div><a href='about_ko.html'>Ko</a></div>" +
+        "<div><a href='about_nicole.html'>Nicole</a></div>" +
+        "<div><a href='about_sara.html'>Sara</a></div>" +
+        "<div><a href='about_shuhei.html'>Shuhei</a></div>" +
+        "</div>"
+    );
+// subNav("Current Tools for Web Design", "current_tool_web_design") +
+// subNav("Current Tools for Web Development", "current_tool_web_dev") +
+// nav("Articles & Resources", "article_resources") +
+// nav("Fture Technologies", "future_tech") +
+// subNav("Web Design Trend", "web_design_trends") +
+// subNav("Web Development Trend", "web_dev_trends") +
+// nav("F.A.Q", "faq") +
+// nav("The Team", "team") +
+// subNav("Ko", "about_ko") +
+// subNav("Nicole", "about_nicole") +
+// subNav("Sara", "about_sara") +
+// subNav("Shuhei", "about_shuhei");
+
+$(function () {
+    // ▼マウスが載ったらサブメニューを表示
+    $("li").mouseenter(function () {
+        $(this).siblings().find(".subNav").hide();  // 兄弟要素に含まれるサブメニューを全部消す。
+        $(this).children().slideDown(150);
+    });
+    // ▼どこかがクリックされたらサブメニューを消す
+    $('html').click(function () {
+        $('.subNav').slideUp(150);
+    });
+});
--- assets/js/commons/nav.js
+++ assets/js/commons/nav.js
@@ -1,5 +1,8 @@
-const nav = (name, link, id) => {
-    return '<li><a href=' + link + '.html' + '>' + name + "<a></li>";
+const nav = (name, link, sub) => {
+    if (sub == undefined) {
+        return '<li><a href=' + link + '.html' + '>' + name + "<a></li>";
+    }
+    return '<li><a href=' + link + '.html' + '>' + name + "<a>" + sub + "</li>";
 }
 
 module.exports = nav;
